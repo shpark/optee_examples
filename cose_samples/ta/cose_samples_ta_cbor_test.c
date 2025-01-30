cec04b2e0e21666ad93ce9161108ca71cfa0f80a
Seonghyun Park <shp4rk@gmail.com>
2025-01-30 08:20:07 +0100

    verify COSE_Sign1 by building TBS hash from it

--- cose_samples/ta/cose_samples_ta_cbor_test.c
+++ cose_samples/ta/cose_samples_ta_cbor_test.c
@@ -18,7 +18,9 @@
 #include "cose_samples_ta_test.h"
 #include "qcbor/UsefulBuf.h"
 #include "qcbor/qcbor_common.h"
+#include "qcbor/qcbor_decode.h"
 #include "qcbor/qcbor_encode.h"
+#include "qcbor/qcbor_spiffy_decode.h"
 #include "t_cose/q_useful_buf.h"
 #include "t_cose_crypto.h"
 #include "t_cose_util.h"
@@ -47,7 +49,7 @@ static inline enum t_cose_err_t encode_cose_sign1_simple(
 }
 
 /**
- * @brief Verify a COSE_Sign1 object usint t_cose APIs
+ * @brief Verify a COSE_Sign1 object using t_cose APIs
  */
 static inline enum t_cose_err_t verify_cose_sign1_simple(
 	struct t_cose_key verification_key,
@@ -256,9 +258,128 @@ static inline enum t_cose_err_t __maybe_unused encode_cose_sign1_with_qcbor(
 	return T_COSE_ERR_FAIL;
 }
 
+/**
+ * @brief Verify a COSE_Sign1 object by manually reconstructing Sig_structure
+ */
+static inline enum t_cose_err_t verify_cose_sign1_by_building_tbs(
+	struct t_cose_key verification_key,
+	struct q_useful_buf_c payload,
+	struct q_useful_buf_c cose)
+{
+	(void)payload;
+
+	QCBORDecodeContext decode_ctx = { };
+	struct q_useful_buf_c protected_parameters = NULL_Q_USEFUL_BUF_C;
+	QCBORItem unprotected_parameters = { };
+	struct q_useful_buf_c payload_from_cose = NULL_Q_USEFUL_BUF_C;
+	struct q_useful_buf_c sig = NULL_Q_USEFUL_BUF_C;
+
+	/* To hold and store TBS hash */
+	Q_USEFUL_BUF_MAKE_STACK_UB(tbs_hash_buffer, 32);
+	struct q_useful_buf_c tbs_hash = NULL_Q_USEFUL_BUF_C;
+
+	QCBORError qcbor_result = QCBOR_SUCCESS;
+	enum t_cose_err_t status = 0;
+
+	QCBORDecode_Init(&decode_ctx, cose, QCBOR_DECODE_MODE_NORMAL);
+
+	QCBORDecode_EnterArray(&decode_ctx, NULL);
+
+	/* Protected parameters */
+	QCBORDecode_EnterBstrWrapped(&decode_ctx,
+				     QCBOR_TAG_REQUIREMENT_NOT_A_TAG,
+				     &protected_parameters);
+
+#if 0
+	/* XXX: This attempt to retrieve `alg` didn't work! */
+	int64_t alg = 0;
+	QCBORDecode_GetInt64InMapN(&decode_ctx,
+				   COSE_HEADER_PARAM_ALG,
+				   &alg);
+
+	IMSG("alg: %ld", alg); /* alg: 0 (although expected -7) */
+#endif
+
+	IMSG("protected parameters:");
+	hexdump1(protected_parameters.ptr, protected_parameters.len);
+
+	QCBORDecode_ExitBstrWrapped(&decode_ctx);
+
+	/* Unproected parameters */
+	QCBORDecode_GetNext(&decode_ctx, &unprotected_parameters);
+
+	/*
+	 * TODO: How to handle QCBORItem? For example, what should do with
+	 * QCBORItem if I know that's a *map* and I'd like to get values out
+	 * of it?
+	 */
+
+	/* Payload */
+	QCBORDecode_EnterBstrWrapped(&decode_ctx,
+				     QCBOR_TAG_REQUIREMENT_NOT_A_TAG,
+				     &payload_from_cose);
+
+	QCBORDecode_ExitBstrWrapped(&decode_ctx);
+
+	IMSG("payload:");
+	hexdump1(payload_from_cose.ptr, payload_from_cose.len);
+
+	/* Signature */
+	QCBORDecode_GetByteString(&decode_ctx, &sig);
+
+	IMSG("sig:");
+	hexdump1(sig.ptr, sig.len);
+
+	QCBORDecode_ExitArray(&decode_ctx);
+
+	qcbor_result = QCBORDecode_Finish(&decode_ctx);
+	if (qcbor_result != QCBOR_SUCCESS) {
+		EMSG("QCBORDecode(cose) qcbor_result=%x", qcbor_result);
+		return T_COSE_ERR_FAIL;
+	}
+
+	DMSG("more steps to go...");
+
+	/* Create TBS with retrieved components */
+	status = create_tbs_hash(T_COSE_ALGORITHM_ES256,
+				 protected_parameters,
+				 NULL_Q_USEFUL_BUF_C,
+				 payload_from_cose,
+				 tbs_hash_buffer,
+				 &tbs_hash);
+	if (status != 0) {
+		EMSG("create_tbs_hash() status=%x", status);
+		return status;
+	}
+
+	IMSG("TBS hash");
+	hexdump1(tbs_hash.ptr, tbs_hash.len);
+
+#if 0
+	/* XXX: What if I tamper with a single byte within signature? */
+	/* Verification fails :) */
+	*((char *)sig.ptr + 32) ^= 0xff;
+#endif
+
+	/* Verify TBS hash against the signature... */
+	status = t_cose_crypto_verify(T_COSE_ALGORITHM_ES256,
+				      verification_key,
+				      NULL_Q_USEFUL_BUF_C,
+				      tbs_hash,
+				      sig);
+	if (status != 0) {
+		EMSG("t_cose_crypto_verify() status=%x", status);
+		return status;
+	}
+
+	IMSG("COSE_Sign1 verification successful!");
+
+	return T_COSE_SUCCESS;
+}
+
 static unsigned char q_buf[2 * 32] = { };
 
-#define PAYLOAD_STRING	"This is content."
+#define PAYLOAD		"This is content."
 
 TEE_Result run_cbor_cose_tests(void)
 {
@@ -266,10 +387,7 @@ TEE_Result run_cbor_cose_tests(void)
 	struct t_cose_key verification_key = { };
 	enum t_cose_err_t status = 0;
 
-	struct q_useful_buf_c payload = {
-		PAYLOAD_STRING,
-		strlen(PAYLOAD_STRING)
-	};
+	struct q_useful_buf_c payload = Q_USEFUL_BUF_FROM_SZ_LITERAL(PAYLOAD);
 
 	/* For `encode_cose_sign1_simple()` */
 	Q_USEFUL_BUF_MAKE_STACK_UB(cose_buffer1, 512);
@@ -338,10 +456,16 @@ TEE_Result run_cbor_cose_tests(void)
 	IMSG("Cose_Sign1 created with create_tbs() helper function...");
 	hexdump1(cose2.ptr, cose2.len);
 
-	/* Verify COSE_Sign1 object created with `create_tbs()` */
-	status = verify_cose_sign1_simple(verification_key, payload, cose2);
+	/*
+	 * Verify COSE_Sign1 by manually decoding it using QCBOR APIs and
+	 * reconstructing TBS hash from it with `create_tbs_hash()` helper
+	 * function.
+	 */
+	status = verify_cose_sign1_by_building_tbs(verification_key,
+						   payload,
+						   cose1);
 	if (status != 0) {
-		EMSG("verify_cose_sign1_simple(cose2) status=%x", status);
+		EMSG("verify_cose_sign1_by_building_tbs() status=%x", status);
 		return TEE_ERROR_GENERIC;
 	}
 
